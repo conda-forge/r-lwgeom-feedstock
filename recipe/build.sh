75de2c28db76630ce9fbb6344c5ee8cf5213adb5
Isuru Fernando <isuruf@gmail.com>
2021-02-17 22:07:26 -0600

    try again

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,8 @@ set -o errexit -o pipefail
 
 export CPPFLAGS="${CPPFLAGS} -Wl,-rpath,${PREFIX}/lib"
 # This is just to get around a configure failure when trying to link to gdal.	  ${R} CMD INSTALL --build .
-if [[ ${target_platform} == osx-64 ]]; then
+LIBS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib"
+if [[ ${target_platform} == osx-* ]] || [[ ${target_platform} == linux-* ]]; then
   LIBRARY_PATH=${PREFIX}/lib
   export CXX="${CXX} --std=c++14 -Wl,-rpath,${PREFIX}/lib"
   export CC="${CC} -Wl,-rpath,${PREFIX}/lib"
